36f331bd2af9741efd50552a2da9a1b75eeeeef4
Ved Suthar <vedhsuthar@gmail.com>
2023-07-19 17:35:55 -0400

    Fixed GUI update
    Previously I spawned new object every frame, fixed it to just update the position

--- src/gui.rs
+++ src/gui.rs
@@ -12,10 +12,13 @@ pub fn run(){
     .add_startup_system(create_sample_world)
     .add_startup_system(spawn_camera)
     .add_startup_system(spawn_directional_light)
+    .add_startup_system(set_initial_state)
     .add_system(update_frame)
     .add_plugins(DefaultPlugins)
     .run(); 
 }
+
+
 
 
 fn create_sample_world(mut world: ResMut<World>){
@@ -25,10 +28,28 @@ fn create_sample_world(mut world: ResMut<World>){
     world.add_vehicle(0.0,0.0,4.0,-7.0,1,250.0,0,311.0);
 }
 
-fn update_frame(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>,mut world: ResMut<World>, time: Res<Time>) {
-    // Clear the previous frame entities
-    commands.insert_resource(DespawnComponents::<PbrBundle>::default());
+#[derive(Component)]
+struct BevyVehicle;
+
+fn update_frame(mut world: ResMut<World>, time: Res<Time>, mut query: Query<(&mut Transform, &mut BevyVehicle)>) {
+
     update_comp(time.delta_seconds(),&mut world);
+
+    let mut i = 0;
+    for (mut t,v) in query.iter_mut() {
+        let veh = &world.vehicles[i];
+        let road_from = Vec3::new(world.roads[veh.on_road].from.0, world.roads[veh.on_road].from.1, world.roads[veh.on_road].from.2);
+        let road_to = Vec3::new(world.roads[veh.on_road].to.0, world.roads[veh.on_road].to.1, world.roads[veh.on_road].to.2);
+        let road_length = (road_to - road_from).length();
+        let position = road_from + (road_to - road_from) * (veh.position/road_length);
+        t.translation = position;
+        i+=1;
+    }
+    
+}
+
+fn set_initial_state(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>,mut world: ResMut<World>){
+    update_comp(0.0,&mut world);
     for road in &world.roads {
         let from_vec3 = Vec3::new(road.from.0, road.from.1, road.from.2);
         let to_vec3 = Vec3::new(road.to.0, road.to.1, road.to.2);
@@ -56,7 +77,7 @@ fn update_frame(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut ma
         let road_length = (road_to - road_from).length();
         let position = road_from + (road_to - road_from) * (vehicle.position/road_length);
         
-        commands.spawn(PbrBundle {
+        commands.spawn((PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Cube { size: vehicle_size })),
             material: materials.add(Color::rgb(0.8, 0.2, 0.2).into()),
             transform: Transform {
@@ -64,15 +85,22 @@ fn update_frame(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut ma
                 ..Default::default()
             },
             ..Default::default()
-        });
+        },BevyVehicle));
     }
 }
 
+#[derive(Component)]
+struct WorldCamera;
 fn spawn_camera(mut commands: Commands){
-    commands.spawn(Camera3dBundle{
-        transform: Transform::from_xyz(0.0, 0.0, 1000.0).looking_at(Vec3::ZERO, Vec3::Y),
+    commands.spawn((Camera3dBundle{
+        transform: Transform{
+            translation: Vec3::new(100.0, -250.0, 500.0),
+            rotation: Quat::from_rotation_x(0.3),
+            ..Default::default()
+        },
         ..Default::default()
-    });
+    },
+    WorldCamera));
 }
 
 
